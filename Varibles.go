da5af1118369c7197e7030e56e19334c60999372
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-03-03 10:59:07 +0530

    Add files via upload

---
+++ Varibles.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Creating a WebServer in Go language"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = false
+	fmt.Println(d)
+
+	f := "apple"
+	fmt.Printf(f)
+
+	// syntax
+	// var variable_name type = value
+	// var age int = 20
+}
