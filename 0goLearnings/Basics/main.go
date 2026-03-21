a25fb6ed0a7b10b50dd5b41b38be7921391d7de0
nalin <nalinikantbastia@gmail.com>
2026-03-21 15:44:18 +0530

    Datatypes and Arithmetic operations in GoLang

---
+++ 0goLearnings/Basics/main.go
@@ -0,0 +1,21 @@
+// Possible arithmetic operations for intigers
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int16 = 170
+	var y int16 = 83
+	//Addition
+	fmt.Printf(" addition :  %d + %d = %d\n ", x, y, x+y)
+	//Subtraction
+	fmt.Printf("subtraction : %d - %d = %d\n", x, y, x-y)
+	//Multiplication
+	fmt.Printf(" multiplication : %d * %d = %d\n", x, y, x*y)
+	//Division
+	fmt.Printf(" division : %d / %d = %d\n", x, y, x/y)
+
+	//Modulus
+	fmt.Printf(" remainder : %d %% %d = %d\n", x, y, x%y)
+}
